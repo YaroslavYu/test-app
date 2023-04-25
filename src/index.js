e2951b8f6632c8ee91322378720591fc7cfb0deb
YaroslavYu <yaroslavyu801@gmail.com>
2023-04-25 17:33:47 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import "./index.css";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from "react-router-dom";
-import { GlobalStyle } from "GlobalStyles";
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'GlobalStyles';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="followers-app">
+    <BrowserRouter basename="test-app">
       <App />
       <GlobalStyle />
     </BrowserRouter>
